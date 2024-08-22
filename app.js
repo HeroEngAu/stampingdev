d3401485af1e3af7a6a1c6f4e80ffe8528240c89
HeroEngAu <mlevkovsky@heroengineering.com.au>
2024-08-22 11:43:46 +0800

    New commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { createCanvas, loadImage } = require('canvas');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 7000;
 
 // Set up EJS
 app.set('view engine', 'ejs');
